d7321de170f8d8aac9321e08a9efe6459bd26796
Phildo <Phildo211@gmail.com>
2017-06-20 13:31:01 -0500

    new direction for ice cube template

---
+++ Assets/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+  GameObject camera_house;
+  new GameObject camera;
+
+  void Start()
+  {
+    camera_house  = GameObject.Find("CameraHouse");
+    camera        = GameObject.Find("Main Camera");
+  }
+
+  void Update()
+  {
+
+  }
+
+}
+
