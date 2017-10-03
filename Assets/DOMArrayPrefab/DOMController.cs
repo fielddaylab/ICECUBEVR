f2732bdae1ae5fea6ebe6ce4c9020f0b7578bb22
Phildo <phildo211@gmail.com>
2017-10-03 16:47:43 -0500

    integrated ice cube nodes

---
+++ Assets/DOMArrayPrefab/DOMController.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DOMController : MonoBehaviour {
+
+    public bool on = false;
+    public float lastCharge = 0.0f;
+    public int stringNum = 0;
+    public int domNum = 0;
+    private GameObject eventSphere = null;
+    private MeshRenderer domGlobe = null;
+    private MeshRenderer domGlobe2 = null;
+    private Shader standard = null;
+    private Shader partAdd = null;
+    private float oldScale = 1.0f;
+
+    private Color orange = new UnityEngine.Color(1.0f, 0.5f, 0.0f, 1.0f);
+    private Color purple = new UnityEngine.Color(0.5f, 0.0f, 1.0f, 1.0f);
+    private Color defaultColor = new UnityEngine.Color(0.5f, 0.5f, 0.5f, 1.0f);
+
+	// Use this for initialization
+	void Start () {
+        /*standard = Shader.Find("Standard");
+        partAdd = Shader.Find("Particles/Additive");
+        GameObject externalParts = transform.FindChild("s1DOM_ExternalParts").gameObject;
+        if (externalParts != null)
+        {
+            domGlobe = externalParts.GetComponent<MeshRenderer>();
+            //r.materials[0].shader = Shader.Find("Particles/Additive");
+            //need to change this on all LOD levels as well too...
+            GameObject shell = externalParts.transform.FindChild("Outer").gameObject;
+            if (shell != null)
+            {
+                domGlobe2 = shell.GetComponent<MeshRenderer>();
+                //r2.materials[2].shader = Shader.Find("Particles/Additive");
+            }
+        }*/
+
+        Transform t = transform.Find("low_poly_sphere");
+		if(t == null)
+		{
+			eventSphere = gameObject;
+		}
+		else
+		{
+			eventSphere = t.gameObject;
+		}
+		
+        oldScale = eventSphere.transform.localScale.x;
+	}
+
+    public void TurnOn(float fTimeFrac, float fRadius)
+    {
+        //Debug.Log("Turning on");
+        //change a material on the globe so that it glows...
+       /* if(domGlobe != null)
+        {
+            domGlobe.materials[0].shader = partAdd;
+        }
+
+        if (domGlobe2 != null)
+        {
+            domGlobe2.materials[2].shader = partAdd;
+        }*/
+
+        if (eventSphere != null)
+        {
+            eventSphere.transform.localScale = new Vector3(fRadius, fRadius, fRadius);
+            float h = (fTimeFrac * 0.69f) - 0.02f;
+            //Debug.Log(h.ToString("F4"));
+            if(h < 0f)
+            {
+                h = 1f + h;
+            }
+
+            //float h = (fTimeFrac * 0.75f);
+            UnityEngine.Color c = UnityEngine.Color.HSVToRGB(h, 1f, 1f);
+           // UnityEngine.Color c2 = UnityEngine.Color.HSVToRGB(h, 0.75f, 0.75f);
+            eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", c);
+            eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", c);
+            
+            /*float fColorFrac = 1.0f / 5.0f;
+			if (fTimeFrac < fColorFrac)
+			{
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", UnityEngine.Color.red);
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", UnityEngine.Color.red);
+			}
+			else if (fTimeFrac < 2.0f * fColorFrac)
+			{
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", orange);
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", orange);
+			}
+			else if (fTimeFrac < 3.0f * fColorFrac)
+			{
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", UnityEngine.Color.yellow);
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", UnityEngine.Color.yellow);
+			}
+			else if (fTimeFrac < 4.0f * fColorFrac)
+			{
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", UnityEngine.Color.green);
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", UnityEngine.Color.green);
+			}
+			else
+			{
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", UnityEngine.Color.blue);
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", UnityEngine.Color.blue);
+			}*/
+			/*else if (fTimeFrac < 6.0f * fColorFrac)
+			{
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", UnityEngine.Color.magenta);
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", UnityEngine.Color.magenta);
+			}
+			else
+			{
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", purple);
+				eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", purple);
+			}*/ 
+        }
+
+        on = true;
+    }
+
+    public void TurnOff()
+    {
+        /*if (domGlobe != null)
+        {
+            domGlobe.materials[0].shader = standard;
+        }
+
+        if (domGlobe2 != null)
+        {
+            domGlobe2.materials[2].shader = standard;
+        }*/
+
+        if(eventSphere != null)
+        {
+            eventSphere.transform.localScale = new Vector3(oldScale, oldScale, oldScale);
+            eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowColor", defaultColor);
+            eventSphere.GetComponent<MeshRenderer>().material.SetColor("_MKGlowTexColor", defaultColor);
+        }
+
+        on = false;
+    }
+}
---
+++ Assets/DOMArrayPrefab/Strings.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class Strings : MonoBehaviour
+{
+  public float offset = 0f;
+  string icecubeFile = "Assets/DOMArrayPrefab/Icecube_Geometry_Data.txt";
+  private const float BELOW_ICE = -1950.0f;
+  private bool first = true;
+
+  //should keep track of all created objects here in a sort of "dom" matrix..
+  //this could allow for very efficient DOM lookups later...
+  private const int NUM_STRINGS = 86;
+  private const int NUM_DOMS_PER_STRING = 64;
+
+  //line updating variables...
+  private bool firstDraw = true;
+  public GameObject stringObject;
+  public Transform cam;
+  public float scaleMultiplier;
+
+  void Start()
+  {
+    StreamReader reader = new StreamReader(icecubeFile);
+
+    LineRenderer lineRen = null;
+    Vector3[] pos = new Vector3[2];
+
+    string line;
+
+    while ((line = reader.ReadLine()) != null)
+    {
+
+      string[] data = line.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+      //grab each column of data
+      string stringIndex = data[0];
+      string domIndex = data[1];
+      string xVal = data[2];
+      string yVal = data[3];
+      string zVal = data[4];
+
+      //parsing each piece of data
+      int domUnitNum = Convert.ToInt32(stringIndex) - 1;
+      int domNum = Convert.ToInt32(domIndex) - 1;
+      float xFloat = float.Parse(xVal);
+      float yFloat = float.Parse(yVal);
+      float zFloat = float.Parse(zVal);
+
+
+      if (domNum <= 60)
+      {
+
+        Vector3 startPos = new Vector3(xFloat + offset, BELOW_ICE + zFloat, yFloat);
+
+        //avgX += xFloat;
+        //avgZ += yFloat;
+        if (domNum == 0)
+        {
+          startPos.y = -50.0f;
+          pos[0] = startPos;
+        }
+
+        if (first == false)
+        {
+          /*lineRen = dom.AddComponent<LineRenderer> ();
+          lineRen.SetWidth (.023f, .023f);
+          lineRen.SetColors (Color.black, Color.black);
+          lineRen.material = new Material (Shader.Find ("Standard"));//Particles/Additive"));
+          lineRen.material.color = Color.black;
+          lineRen.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+          lineRen.receiveShadows = false;
+
+          Vector3[] pos2 = new Vector3[2];
+          pos2 [0] = pos [0];
+          pos2 [1] = pos [1];
+
+          pos2 [0].y += 0.64f;
+          pos2 [1].y -= 0.62f;
+          lineRen.SetPositions (pos2);*/
+        }
+        else
+        {
+          /* lineRen = dom.AddComponent<LineRenderer>();
+           lineRen.SetWidth(.023f, .023f);
+           lineRen.SetColors(Color.black, Color.black);
+           lineRen.material = new Material(Shader.Find("Standard"));//Particles/Additive"));
+           lineRen.material.color = Color.black;
+           lineRen.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+           lineRen.receiveShadows = false;
+
+           Vector3[] pos2 = new Vector3[2];
+           pos2[0] = new Vector3(xFloat, 0.0f, yFloat);
+           pos2[1] = pos[0];
+
+           pos2[1].y += 0.64f;
+           //pos2[1].y -= 0.62f;
+           lineRen.SetPositions(pos2);*/
+        }
+
+         if (domNum == 59)
+         {
+           GameObject stringObj = (GameObject)Instantiate(stringObject); //GameObject.CreatePrimitive(PrimitiveType.Sphere);
+           Vector3 domPos = new Vector3(xFloat + offset, BELOW_ICE + zFloat, yFloat);
+           stringObj.transform.position = domPos;
+           stringObj.transform.SetParent(transform);
+           stringObj.layer = transform.gameObject.layer;
+
+           //how to make line widths different for oculus vs. regular user?
+           pos[1] = new Vector3(xFloat + offset, BELOW_ICE + zFloat, yFloat);
+           lineRen = stringObj.AddComponent<LineRenderer>();
+           lineRen.SetWidth(.023f, .023f);
+           lineRen.SetColors(Color.black, Color.black);
+           lineRen.material = new Material(Shader.Find("Standard"));//Particles/Additive"));
+           lineRen.material.color = Color.black;
+           lineRen.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+           lineRen.receiveShadows = false;
+           lineRen.castShadows = false;
+
+           //Vector3[] pos2 = new Vector3[2];
+           //pos2[0] = new Vector3(xFloat, 0.0f, yFloat);
+           //pos2[1] = pos[0];
+
+           //pos2[1].y += 0.64f;
+           //pos2[1].y -= 0.62f;
+           lineRen.SetPositions(pos);
+         }
+        pos[1] = pos[0];
+        first = false;
+      }
+      else
+      {
+        first = true;
+      }
+
+    }   //end while()
+
+    reader.Close();
+  }//end Start()
+
+  void Update()
+  {
+    //if (firstDraw || cam.velocity.magnitude > 0.0f)
+    {
+      firstDraw = false;
+      for (int i = 0; i < transform.childCount; ++i)
+      {
+        LineRenderer d = transform.GetChild(i).gameObject.GetComponent<LineRenderer>();
+        if(d != null)
+        {
+          Vector3 vCam = cam.position;
+          vCam.y = 0.0f;
+          Vector3 lPos = d.transform.position;
+          lPos.y = 0.0f;
+          float w = Vector3.Distance(vCam, lPos);
+          if (w > 20.0f)
+          {
+            w *= scaleMultiplier;
+          }
+          else
+          {
+            w = 0.023f;
+          }
+
+          d.startWidth = w;
+          d.endWidth = w;
+        }
+      }
+    }
+  }//end Update()
+}//end Class
