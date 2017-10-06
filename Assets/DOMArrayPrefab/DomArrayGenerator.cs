b6e44031254367f1a1de3b9b08ec7cdd1c7f09df
Phildo <phildo211@gmail.com>
2017-10-06 13:46:01 -0500

    simplified to understand dom

--- Assets/DOMArrayPrefab/DomArrayGenerator.cs
+++ Assets/DOMArrayPrefab/DomArrayGenerator.cs
@@ -9,8 +9,6 @@ public class DomArrayGenerator : MonoBehaviour {
   public string geometryFile = "Icecube_Geometry_Data.txt";
   private const float BELOW_ICE = -1950.0f;
 
-  //should keep track of all created objects here in a sort of "dom" matrix..
-  //this could allow for very efficient DOM lookups later...
   private const int NUM_STRINGS = 86;
   private const int NUM_DOMS_PER_STRING = 64;
 
@@ -23,72 +21,53 @@ public class DomArrayGenerator : MonoBehaviour {
     Vector3[] pos = new Vector3[2];
 
     string line;
-    /*float avgX = 0.0f;
-      float avgZ = 0.0f;
-      float count = 0.0f;*/
-    while((line = reader.ReadLine()) != null) {
-
+    while((line = reader.ReadLine()) != null)
+    {
       string[] data = line.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
-      //grab each column of data
       string stringIndex = data [0];
       string domIndex = data [1];
       string xVal = data [2];
       string yVal = data [3];
       string zVal = data [4];
 
-      //parsing each piece of data
       int domUnitNum = Convert.ToInt32(stringIndex)-1;
       int domNum = Convert.ToInt32(domIndex)-1;
       float xFloat = float.Parse(xVal);
       float yFloat = float.Parse (yVal);
       float zFloat = float.Parse (zVal);
 
-      //create DOMS
-      GameObject tableDom = (GameObject)Instantiate(domObject); //GameObject.CreatePrimitive(PrimitiveType.Sphere);
-      //SetLayersRecursively(tableDom, LayerMask.NameToLayer("TableTop"));
+      GameObject tableDom = (GameObject)Instantiate(domObject);
 
-      Vector3 domPos = new Vector3 (xFloat, BELOW_ICE + zFloat, yFloat);
+      Vector3 domPos = new Vector3(xFloat, BELOW_ICE + zFloat, yFloat);
 
-      //table
       tableDom.transform.position = domPos;
-      tableDom.transform.SetParent (transform);
-      //Debug.Log("Setting scale");
+      tableDom.transform.SetParent(transform);
       tableDom.transform.localScale.Set(10.0f, 10.0f, 10.0f);
       tableDom.GetComponent<DOMController>().stringNum = domUnitNum;
       tableDom.GetComponent<DOMController>().domNum = domNum;
 
-
       if(domNum <= 60)
       {
-        Vector3 startPos = new Vector3 (xFloat, BELOW_ICE + zFloat, yFloat);
-
-        //avgX += xFloat;
-        //avgZ += yFloat;
-        if(domNum == 0)
-        {
-          pos[0] = startPos;
-        }
-        pos [1] = pos [0];
+        if(domNum == 0) pos[0] = new Vector3(xFloat, BELOW_ICE + zFloat, yFloat);
+        pos[1] = pos[0];
       }
 
       DOMArray[domUnitNum, domNum] = tableDom;
-    }   //end while()
-
-    reader.Close ();
-  }//end Start()
-
-  private void SetLayersRecursively(GameObject obj, int layer) {
-    if (obj == null) {
-      return;
     }
 
+    reader.Close();
+  }
+
+  private void SetLayersRecursively(GameObject obj, int layer)
+  {
+    if(!obj) return;
     obj.layer = layer;
-    foreach (Transform child in obj.transform) {
-      if (child == null) {
-        continue;
-      }
-      SetLayersRecursively (child.gameObject, layer);
+    foreach(Transform child in obj.transform)
+    {
+      if(!child) continue;
+      SetLayersRecursively(child.gameObject, layer);
     }
   }
-}//end Class
+}
+
--- Assets/DOMArrayPrefab/Strings.cs
+++ Assets/DOMArrayPrefab/Strings.cs
@@ -8,14 +8,10 @@ public class Strings : MonoBehaviour
   public float offset = 0f;
   string icecubeFile = "Assets/DOMArrayPrefab/Icecube_Geometry_Data.txt";
   private const float BELOW_ICE = -1950.0f;
-  private bool first = true;
 
-  //should keep track of all created objects here in a sort of "dom" matrix..
-  //this could allow for very efficient DOM lookups later...
   private const int NUM_STRINGS = 86;
   private const int NUM_DOMS_PER_STRING = 64;
 
-  //line updating variables...
   public GameObject stringObject;
   public Transform cam;
   public float scaleMultiplier;
@@ -29,83 +25,38 @@ public class Strings : MonoBehaviour
 
     string line;
 
-    while ((line = reader.ReadLine()) != null)
+    while((line = reader.ReadLine()) != null)
     {
-
       string[] data = line.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
-      //grab each column of data
       string domIndex = data[1];
       string xVal = data[2];
       string yVal = data[3];
       string zVal = data[4];
 
-      //parsing each piece of data
       int domNum = Convert.ToInt32(domIndex) - 1;
       float xFloat = float.Parse(xVal);
       float yFloat = float.Parse(yVal);
       float zFloat = float.Parse(zVal);
 
-
-      if (domNum <= 60)
+      if(domNum <= 60)
       {
-
         Vector3 startPos = new Vector3(xFloat + offset, BELOW_ICE + zFloat, yFloat);
 
-        //avgX += xFloat;
-        //avgZ += yFloat;
-        if (domNum == 0)
+        if(domNum == 0)
         {
           startPos.y = -50.0f;
           pos[0] = startPos;
         }
 
-        if (first == false)
+        if(domNum == 59)
         {
-          /*lineRen = dom.AddComponent<LineRenderer> ();
-            lineRen.SetWidth (.023f, .023f);
-            lineRen.SetColors (Color.black, Color.black);
-            lineRen.material = new Material (Shader.Find ("Standard"));//Particles/Additive"));
-            lineRen.material.color = Color.black;
-            lineRen.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-            lineRen.receiveShadows = false;
-
-            Vector3[] pos2 = new Vector3[2];
-            pos2 [0] = pos [0];
-            pos2 [1] = pos [1];
-
-            pos2 [0].y += 0.64f;
-            pos2 [1].y -= 0.62f;
-            lineRen.SetPositions (pos2);*/
-        }
-        else
-        {
-          /* lineRen = dom.AddComponent<LineRenderer>();
-             lineRen.SetWidth(.023f, .023f);
-             lineRen.SetColors(Color.black, Color.black);
-             lineRen.material = new Material(Shader.Find("Standard"));//Particles/Additive"));
-             lineRen.material.color = Color.black;
-             lineRen.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-             lineRen.receiveShadows = false;
-
-             Vector3[] pos2 = new Vector3[2];
-             pos2[0] = new Vector3(xFloat, 0.0f, yFloat);
-             pos2[1] = pos[0];
-
-             pos2[1].y += 0.64f;
-          //pos2[1].y -= 0.62f;
-          lineRen.SetPositions(pos2);*/
-        }
-
-        if (domNum == 59)
-        {
-          GameObject stringObj = (GameObject)Instantiate(stringObject); //GameObject.CreatePrimitive(PrimitiveType.Sphere);
+          GameObject stringObj = (GameObject)Instantiate(stringObject);
           Vector3 domPos = new Vector3(xFloat + offset, BELOW_ICE + zFloat, yFloat);
           stringObj.transform.position = domPos;
           stringObj.transform.SetParent(transform);
           stringObj.layer = transform.gameObject.layer;
 
-          //how to make line widths different for oculus vs. regular user?
           pos[1] = new Vector3(xFloat + offset, BELOW_ICE + zFloat, yFloat);
           lineRen = stringObj.AddComponent<LineRenderer>();
           AnimationCurve curve = new AnimationCurve();
@@ -115,57 +66,40 @@ public class Strings : MonoBehaviour
           lineRen.widthCurve = curve;
           lineRen.startColor = Color.black;
           lineRen.endColor = Color.black;
-          lineRen.material = new Material(Shader.Find("Standard"));//Particles/Additive"));
+          lineRen.material = new Material(Shader.Find("Standard"));
           lineRen.material.color = Color.black;
           lineRen.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
           lineRen.receiveShadows = false;
 
-          //Vector3[] pos2 = new Vector3[2];
-          //pos2[0] = new Vector3(xFloat, 0.0f, yFloat);
-          //pos2[1] = pos[0];
-
-          //pos2[1].y += 0.64f;
-          //pos2[1].y -= 0.62f;
           lineRen.SetPositions(pos);
         }
         pos[1] = pos[0];
-        first = false;
       }
-      else
-      {
-        first = true;
-      }
-
-    }   //end while()
+    }
 
     reader.Close();
-  }//end Start()
+  }
 
   void Update()
   {
-    for (int i = 0; i < transform.childCount; ++i)
+    for(int i = 0; i < transform.childCount; ++i)
     {
       LineRenderer d = transform.GetChild(i).gameObject.GetComponent<LineRenderer>();
-      if(d != null)
+      if(d)
       {
         Vector3 vCam = cam.position;
         vCam.y = 0.0f;
         Vector3 lPos = d.transform.position;
         lPos.y = 0.0f;
         float w = Vector3.Distance(vCam, lPos);
-        if (w > 20.0f)
-        {
-          w *= scaleMultiplier;
-        }
-        else
-        {
-          w = 0.023f;
-        }
+        if(w > 20.0f) w *= scaleMultiplier;
+        else          w = 0.023f;
 
         d.startWidth = w;
         d.endWidth = w;
       }
     }
 
-  }//end Update()
-}//end Class
+  }
+}
+
